af8e9dba48bc62651688ef49d0c4ad33bc4e4326
Zoe <mariagiargei031@gmail.com>
2022-07-19 19:33:51 -0300

    incorporé algunos eventos

--- script/script.js
+++ script/script.js
@@ -34,13 +34,13 @@
  4.Modifico desde el CSS la clase gastos
 */
 
-const gastos = []
+const gastos = [] //Array de gastos del usuario
 
 let presupuesto = 0
 const inputPresupuesto = document.getElementById(`inputPresupuesto`)
 const btnEnviarPresu = document.getElementById(`btnEnviarPresu`)
 
-
+// Cargamos el presupuesto
 btnEnviarPresu.addEventListener(`click`, () => {
 
     presupuesto = 0
@@ -49,35 +49,61 @@ btnEnviarPresu.addEventListener(`click`, () => {
     inputPresupuesto.value = null
 })
 
-let titulo,categoria,monto=0,posicion=0
-const btnEnviarGasto = document.getElementById(`btnEnviarGasto`)
 
+
+let titulo,categoria,monto=0
+const btnEnviarGasto = document.getElementById(`btnEnviarGasto`)
 
 const inputTitulo = document.getElementById(`inputTitulo`)
 const inputCategoria = document.getElementById(`inputCategoria`)
 const inputMonto = document.getElementById(`inputMonto`)
 
 
-const idNoCargoPresupuesto = document.getElementById(`idNoCargoPresupuesto`) //no funcion aún
+const divNoCargoPresupuesto = document.getElementById(`divNoCargoPresupuesto`) //no funcion aún
 
+//let tagMensaje, mensaje
+let mensaje, mensajeAlerta
+
+//Cargamos gastos del usuario
 btnEnviarGasto.addEventListener(`click`, () => {
 
-    titulo = inputTitulo.value
-    categoria = inputCategoria.value
-    monto = inputMonto.value
+    if(presupuesto==0){
 
-    console.log(titulo,categoria,monto) //prueba
+        if(divNoCargoPresupuesto.hasChildNodes()){
+            divNoCargoPresupuesto.removeChild(divNoCargoPresupuesto.lastChild)
+        } else{
 
+            mensaje = `No puede ingrear un gasto sin antes cargar el presupuesto`
+            mensajeAlerta = document.createTextNode(`${mensaje}`)
+            divNoCargoPresupuesto.appendChild(mensajeAlerta)
+        }
 
-    const gasto = new Gasto(titulo,categoria,monto,(new Date()).toLocaleDateString())
-    gastos.push(gasto)
+    } else{
 
-
-    console.log(gastos) //prueba
-
-    inputTitulo.value = null
-    inputCategoria.value = null
-    inputMonto.value = null
+        titulo = inputTitulo.value
+        categoria = inputCategoria.value
+        monto = inputMonto.value
+    
+        console.log(titulo,categoria,monto) //prueba
+    
+    
+        const gasto = new Gasto(titulo,categoria,monto,(new Date()).toLocaleDateString())
+        gastos.push(gasto)
+    
+        presupuesto = presupuesto - monto
+    
+    
+        console.log(`Presupuesto: ${presupuesto}`) //prueba
+        console.log(gastos) //prueba
+    
+        inputTitulo.value = null
+        inputCategoria.value = null
+        inputMonto.value = null
+    
+        if(divMostrarPresupuesto.hasChildNodes()){
+            divMostrarPresupuesto.removeChild(divMostrarPresupuesto.lastChild)
+        }
+    }
 
 })
 
@@ -87,7 +113,7 @@ const divMostrarGastos = document.getElementById(`divMostrarGastos`)
 
 btnMostrarGastos.addEventListener(`click`, () => {
 
-    gastos.forEach((gasto) => {
+    gastos.forEach((gasto) => { //todavía no se bien como hacer para mostrar solo una vez cada gasto
         
         divMostrarGastos.innerHTML += `
         
@@ -102,3 +128,19 @@ btnMostrarGastos.addEventListener(`click`, () => {
     })
 
 })
+
+const divMostrarPresupuesto = document.getElementById(`divMostrarPresupuesto`)
+const btnMostrarPresupuesto = document.getElementById(`btnMostrarPresupuesto`)
+
+
+btnMostrarPresupuesto.addEventListener(`click`, () => {
+
+    if(divMostrarPresupuesto.hasChildNodes()){
+        divMostrarPresupuesto.removeChild(divMostrarPresupuesto.lastChild)
+    } else{
+
+        let nuevoPresupuesto = document.createTextNode(`$ ${presupuesto}`)
+        divMostrarPresupuesto.appendChild(nuevoPresupuesto)
+    }
+})
+
